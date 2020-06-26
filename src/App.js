9715c6adf5d63c594fc69cff744d393097cba6a6
akshay <akshay.khobragade7875@gmail.com>
2020-06-26 20:49:33 +0530

    Created canvas and defiend canvasRef

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import { useInterval } from './useInterval'
 import './App.scss';
+import * as constant from './Constants';
 
-function App() {
+const App = () => {
+  const canvasRef = useRef();
+
   return (
-    <div className="App">
-      venom snake
+    <div className="App" role="button" tabIndex="0">
+      <canvas 
+        className="canvas" 
+        ref={canvasRef}
+        width={`${constant.canvas_size[0]}`}
+        height={`${constant.canvas_size[1]}`}
+      />
     </div>
   );
 }
